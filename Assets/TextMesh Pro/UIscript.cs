b985a2c2c29f37bffa564ffc6c408514b6d02889
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2025-05-05 09:05:06 -0300

    afcuoha

---
+++ Assets/TextMesh Pro/UIscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPpro;
+
+public class UIscript : MonoBehaviour
+{
+    public TextMeshProUGUI TXTnombre;
+    void Start()
+    {
+        TXTnombre.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
